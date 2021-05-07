7859548f36ff5cb62cc7b80363379e50ce316b1f
Samantha walker <walker24084@lcsd2kids.org>
2021-05-07 10:45:53 -0600

    the grid works now

--- script.js
+++ script.js
@@ -2,16 +2,41 @@
 const gridContainer = document.querySelector("#gridContainer")
 
 
-let numOfCells = 16**2;
+let numOfCells = 150**2;
 
 for (let i = 0; i < numOfCells; i++){
     const singleCell = document.createElement("div")
-    singleCell.textContent = "hello";
+    singleCell.textContent = " . ";
     singleCell.style.color ="white";
-    singleCell.classList.add("bg-red");
+    singleCell.classList.add("bg-black");
     singleCell.addEventListener('mouseover', () => {
-        singleCell.textContent = "goodbye";
-        singleCell.classList.toggle("bg-purple");
+        singleCell.textContent = "   ";
+        
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor =  `rgb( ${x}, ${y}, ${z} )`;
+    
+        singleCell.style.background = bgColor;
+        
     });
     gridContainer.appendChild(singleCell);
+}
+
+function makeRows(rows, cols){
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
+    
+        clearInterval.innerText = (i + 1);
+        clearInterval.classList.add("grid-item");
+};
+makeRows(150 , 150);
+function random_bg_color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor =  `rgb( ${x}, ${y}, ${z} )`;
+    document.body.innerText = bgColor;
+
+    document.body.style.background = bgColor;
 }
